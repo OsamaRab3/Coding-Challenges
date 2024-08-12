3658fd536d21bbf8108d4d9598f0a4d95a7875ad
osama <osrab3@gmail.com>
2024-08-12 10:58:00 +0300

    URL Shortener using bash

---
+++ URL Shortener/short.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo -e "\e[32;1m" 
+
+figlet "URL Shortener"
+
+echo -e "\e[0m"
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+long_url=$1
+short_url=$(curl -s https://is.gd/create.php\?format\=simple\&url\=$long_url)
+
+if [[ $short_url == "Error:"* ]]; then
+    echo -e "\e[31mFailed to shorten URL. Please check the input URL.\e[0m"
+else
+     echo -e "Shortened URL:\e[32m $short_url\e[0m"
+fi
+
